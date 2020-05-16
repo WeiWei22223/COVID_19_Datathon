def3fc96de5fd3882f2c1b56f05905db1e6b7bc5
qiaoxl <qiaoxl@uw.edu>
2020-05-16 14:55:09 -0700

    create rough ui.r

--- shinyapp/ui.R
+++ shinyapp/ui.R
@@ -2,15 +2,22 @@ library(shiny)
 library(shinythemes)
 library(plotly)
 
+#main_page <- tabPanel()
+
+bar_plot_states <- tabPanel(
+  "Bar Plot for States",
+  h1("Bar Plot", align = "center"),
+  plotOutput("barplot")
+)
+
 my_ui <- fluidPage(
   theme = shinythemes::shinytheme("readable"),
-
+  HTML('<center><h1>COVID-19 IN INDIA</h1></center>'),
   headerPanel("Data Visulization"),
-  
-  sidebarPanel(),
-  
-  mainPanel(
-    h1("Bar Plot", align = "center"),
-    plotOutput("barplot")
+  #sidebarPanel(),
+  navbarPage(
+    "COVID-19 IN INDIA", 
+    bar_plot_states
+    #diversity_map              
   )
 )
